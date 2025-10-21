375b8a4f0861abe195615642cfb01a8c3854a9a2
PatriciaTenda <patricia.tdjouala@gmail.com>
2025-10-21 14:50:49 +0200

    refactor : mise à jour du  fichier utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 
+# Définir le chemin de base et créer le dossier 'data' s'il n'existe pas
 base = Path(__file__).resolve().parents[1]
+print(f"Base path: {base}")
 data_path = base/"data"
 if not data_path.exists():
     data_path.mkdir(parents=True)
+
+#Définir le chemin de base et créer le dossier 'infra-mlflow' s'il n'existe pas
+base = Path(__file__).resolve().parents[2]
+print(f"Base path: {base}")
+data_path = base/"infra-mlflow"
+if not data_path.exists():
+    data_path.mkdir(parents=True)
+
+
+
+
+
