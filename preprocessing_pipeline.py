d4e49feb51eddbdbc20667d14ece69fc661c6d4b
promise-temi <temi.promisejohn@gmail.com>
2025-10-23 11:57:56 +0200

    code mis a jours

--- preprocessing_pipeline.py
+++ preprocessing_pipeline.py
@@ -3,12 +3,10 @@ import pandas as pd
 
 
 
-
-
 # Réccupération d'uniquement les collones remplies
 
 emptyness_infos = []
-df = pd.read_parquet('outputs/all_immo_entries.parquet')
+df = pd.read_parquet('all_immo_entries.parquet')
 # nombre total de lignes
 total_rows = df.shape[0] 
 # liste des colonnes
@@ -37,22 +35,20 @@ assez_rempli = []
 for index, row in enumerate(df_emptyness.iterrows()):
     current_column = df_emptyness.iloc[index]
     # si pourcentage de vide superieur a 96% ajout a la liste 'trop vide'
-    if current_column['pourcentage_vide'] == 0:
+    if current_column['pourcentage_vide'] <= 96:
         rempli.append(current_column['name'])
 
     # si pourcentage de vide egal à 100% ajout a la liste 'completement vide'
     if current_column['pourcentage_vide'] == 100 :
         completement_vide.append(current_column['name'])
     
-    if current_column['pourcentage_vide'] < 100 :
-        assez_rempli.append(current_column['name'])
 
 
 
 
 print(len(assez_rempli))
 
-df[assez_rempli].to_parquet('outputs/all_immo_no_empty.parquet')
+df[rempli].to_parquet('all_immo_no_empty.parquet')
 
 
 
@@ -61,7 +57,7 @@ df[assez_rempli].to_parquet('outputs/all_immo_no_empty.parquet')
 
 
 
-df = pd.read_parquet('outputs/all_immo_no_empty.parquet')
+df = pd.read_parquet('all_immo_no_empty.parquet')
 
 
 # identification of different dtypes
@@ -85,13 +81,9 @@ df_no_nan = df.fillna(value=0)
 
 # drop all objects (not very pertinant insights)
 df_final = df_no_nan.drop(columns=objects)
-
+df_final = df_final.drop_duplicates()
 # drop the remaining useless columns
-df_final = df_final.drop(columns=['Unnamed: 0','No voie','Prefixe de section','No disposition','Type local','Surface reelle bati'])
-
-# 
-df_final.to_parquet('outputs/features_immo_data.parquet')
+df_final = df_final.drop(columns=['Unnamed: 0','No voie','Prefixe de section','No disposition','Surface reelle bati'])
 
 
-
-
+df_final.to_parquet('features_immo_data.parquet')
