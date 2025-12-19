47bedd8ff80ee2529e9875c6c143beb8c3cf7fdf
PatriciaTenda <patricia.tdjouala@gmail.com>
2025-12-19 07:57:24 +0100

    fix : main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from schemas.propertyFeatures import PropertyPriceRequest, PropertyPriceResponse
+from fastapi.templating import Jinja2Templates 
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
 #from artefacts.models import model
 
 # Create FastAPI instance
 app = FastAPI(title="Patricia Promise Immo API", version="1.0",
               description="API pour la prédiction du prix des biens immobiliers.")
 
-# Define a home page route
-@app.get("/", tags=["Page d'accueil"])
-def home_page():
-    return {"Message" : "Welcome to Patricia Promise Immo API!"}
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Set up Jinja2 templates
+template = Jinja2Templates(directory="templates")
+
+# Define a home page route
+@app.get("/", response_class=HTMLResponse, tags=["Page d'accueil"])
+def formPageToPredict(request: Request):
+    return template.TemplateResponse("index.html", {"request" : request})
+                                     
 
 @app.post("/predict", tags=["Prédiction du prix immobilier"])
-def priceImmoPrediction(inputForModel:PropertyPriceRequest, include_confidence_score: bool = True):
+def priceImmoPrediction(request:Request, inputForModel:PropertyPriceRequest, include_confidence_score: bool = True):
+    """Endpoint pour prédire le prix d'un bien immobilier en fonction de ses caractéristiques."""
+
+    confidence_score = None
+
     features = [inputForModel.nombre_de_lots,
                 inputForModel.code_type_local,
                 inputForModel.nombre_pieces_principales,
@@ -24,10 +38,18 @@ def priceImmoPrediction(inputForModel:PropertyPriceRequest, include_confidence_s
    
     pricePrédiction =  0 #model.predict([features])[0]
 
+    response_predict = PropertyPriceResponse(
+        pricePrediction=pricePrédiction,
+        currency="EUR",
+        confidence_score= confidence_score
+    )
+
     if include_confidence_score:
         confidence_score = 0 #model.predict_proba([features])[0].max()
 
-    return {"Prediction": pricePrédiction,
-            "currency": "EUR",
-            "confidence_score": confidence_score}
+    return template.TemplateResponse(
+        "reponsePredict.html",
+        {"request": request, "response_predict": response_predict}
+    )
+
 
